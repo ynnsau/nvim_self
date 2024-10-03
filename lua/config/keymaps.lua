2a478fb34df845f8efdfb76774572735df97534d
Douglas@SPR2 <958294931@qq.com>
2024-10-03 15:10:45 -0500

    adding all configs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keyvim.keymap.dels are automatically loaded on the VeryLazy event
+-- Default keyvim.keymap.dels that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keyvim.keymap.dels.lua
+-- Add any additional keyvim.keymap.dels here
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
+vim.keymap.del("v", "<A-j>")
+vim.keymap.del("v", "<A-k>")
